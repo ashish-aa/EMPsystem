6203d922be54f42735f12fbfce2e1cd538364568
ashishawale <ashawale3194@gmail.com>
2021-11-30 12:18:42 +0530

    30Nov2021

--- src/LoginAndReg/Main.java
+++ src/LoginAndReg/Main.java
@@ -11,6 +11,7 @@ public class Main{
      static int passWord;
      static ResultSet loginData;
      static register r1;
+     static String queryForLogin ;
     public static void main(String[] args) throws SQLException {
          l1 = new login();
        //  new register();
@@ -20,14 +21,14 @@ public class Main{
         String url = "jdbc:mysql://127.0.0.1:3306/MicroProject";
         String username = "root";
         String password = "Ashish@2003";
-        String queryForLogin = "select * from logReg";
+        
         String queryForReg = "insert into logReg(Username,Password) value()";
 
         //creating connection
 
         Connection con = DriverManager.getConnection(url,username,password);
         Statement st = con.createStatement();
-        loginData = st.executeQuery(queryForLogin);
+        
 
 
         //added action event for login page submit buttton to submit values entered and evaluate further process
@@ -41,36 +42,33 @@ public class Main{
         l1.array = l1.passwordTextfield.getPassword();
         l1.passWordValue = charArrayToInteger(l1.array);
 
+        queryForLogin ="select * from logReg where Username='"+l1.userNameValue+"' and Password ='"+l1.passWordValue+"'";
+
         System.out.println(l1.userNameValue);
         System.out.println(l1.passWordValue);
+        try {
+            loginData = st.executeQuery(queryForLogin);
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
         
         
             //checking whether data entered and data from database matches or not 
-            try {
-                while(loginData.next())
-                {
-                    //retriving data from database and string them in local variables to be chekced with data entered
-                    userName = loginData.getString(2);
-                    passWord = loginData.getInt(3);
-                    //actucal checking
-                    if(l1.userNameValue.equals(userName)&& l1.passWordValue == passWord)
-                    {
-                        System.out.println("You have Successfully logged in !!");
-                        break;
-                    }
-                    else
-                    {
-                        System.out.println("Username or Password does not match.");
-                        break;
-                    }
-
-
-                }
-            } catch (SQLException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
+            
+        try {
+            if(loginData.next())
+            {
+                System.out.println("Logged in Successfully !!");
             }
-        
+            else{
+                System.out.println("Username or Password doesn't match.");
+            }
+            
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
         
     }
 
@@ -90,7 +88,7 @@ public class Main{
     
 }
 // as password field supports getPassword method to return value entered which returns value as char[]. 
-//such data cannot be directly casted to int, so at first it has to be converted into string and then typecasted into an integer. 
+//such data cannot be directly casted to int, so at first it has to be convert into string and then typecasted into an integer. 
 public static int charArrayToInteger(char[] array){
     String arr = new String(array);
     int number = Integer.parseInt(arr);
