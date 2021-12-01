4f55c38beb342457f022a7d927322be6de89d422
ashishawale <ashawale3194@gmail.com>
2021-12-01 16:20:47 +0530

    1Dec2021

---
+++ src/LoginAndReg/EmpInfo.java
@@ -0,0 +1,12 @@
+package LoginAndReg;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.*;
+
+public class EmpInfo {
+    
+}
